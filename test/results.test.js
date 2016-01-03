4412ecf46894476995a81d2df4f5056bea9feeca
Andrew Guldman <aguldman@fluid.com>
2016-01-02 16:24:58 -0800

    tiebreakers

--- test/results.test.js
+++ test/results.test.js
@@ -5,7 +5,7 @@ var
 describe("results", function () {
   it("should work", function (done) {
     this.timeout(10000);
-    createResults("tjfs", 2016, "sb", "BOARDM15-18",
+    createResults("tjfs", 2016, "sb", "M11-14",
     function handleResults(err, results) {
       if (err) {
         throw err;
