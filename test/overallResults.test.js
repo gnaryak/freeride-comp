d2c66115650999a400ff93b4256bff24728e111a
Andrew Guldman <aguldman@fluid.com>
2016-02-29 17:54:29 -0800

    array, csv, and file format options for overall results

--- test/overallResults.test.js
+++ test/overallResults.test.js
@@ -5,14 +5,14 @@ var
 
 describe("overallResults", function () {
 
-  it("should work", function (done) {
+  it("format: array", function (done) {
     this.timeout(10000);
-    createOverallResults("tjfs", 2016, "M11-14", {format: "array"},
+    createOverallResults("tjfs", 2016, "F11-14", {format: "array"},
     function handleResults(err, results) {
       if (err) {
         throw err;
       }
-      console.log("handleResults in test");
+      console.log("handleResults: %j", results);
       // Format should be:
       // [
       // [order, bib, name, team, pts from comp1, ..., pts from compN, total pts]
@@ -29,6 +29,41 @@ describe("overallResults", function () {
         //   expect(r[ix]).to.be.a("number");
         // }
       });
+      done();
+    });
+  });
+
+  it("format: file", function (done) {
+    this.timeout(10000);
+    createOverallResults("tjfs", 2016, "M11-14", {format: "file"},
+    function handleResults(err, results) {
+      if (err) {
+        throw err;
+      }
+      console.log("handleResults: %j", results);
+      // Format should be:
+      // [
+      // [order, bib, name, team, pts from comp1, ..., pts from compN, total pts]
+      // ]
+      expect(results).to.be("overallResults-M11-14.csv");
+      done();
+    });
+  });
+
+  it("format: csv (default)", function (done) {
+    this.timeout(10000);
+    createOverallResults("tjfs", 2016, "BOARD11-14", {},
+    function handleResults(err, results) {
+      if (err) {
+        throw err;
+      }
+      console.log("handleResults: %j", results);
+      // Format should be:
+      // [
+      // [order, bib, name, team, pts from comp1, ..., pts from compN, total pts]
+      // ]
+      expect(results).to.be.a("string");
+      expect(results.length).to.be.above(500);
       done();
     });
   });
