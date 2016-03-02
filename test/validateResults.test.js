96f35d47c65281d436cff1ee8f9b51d2d3c46732
Andrew Guldman <aguldman@fluid.com>
2016-03-02 05:58:08 -0800

    create csv of validation results

--- test/validateResults.test.js
+++ test/validateResults.test.js
@@ -4,18 +4,37 @@ var
   validateResults = require("../lib/validateResults.js");
 
 describe("validateResults", function () {
-  it("should work", function (done) {
+
+  it("format: array", function (done) {
     this.timeout(20000);
-    validateResults("tjfs", 2016, "sb", "Sunday",
+    validateResults("tjfs", 2016, "sb", "Sunday", {format: "array"},
     function handleStartList(err, errors) {
       if (err) {
         throw err;
       }
-      console.log("Errors:");
+      console.log("Errors: %j", errors);
+      expect(errors).to.be.an("array");
       _.each(errors, function (e) {
-        console.log(e.message);
+        console.log("bib: %d, division: %s, msg: %s", e.bib, e.division, e.message);
+        expect(e).to.be.an(Error);
+        expect(e.athlete).to.be.an("object");
+        expect(e.name).to.be.a("string");
+        expect(e.division).to.be.a("string");
+        expect(e.bib).to.be.ok();
       });
-      // expect(response).to.be.a("string");
+      done();
+    });
+  });
+
+  it("format: csv (default)", function (done) {
+    this.timeout(20000);
+    validateResults("tjfs", 2016, "sb", "Sunday", null,
+    function handleStartList(err, csv) {
+      if (err) {
+        throw err;
+      }
+      console.log("Errors: %j", csv);
+      expect(csv).to.be.a("string");
       done();
     });
   });
